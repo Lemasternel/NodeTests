a05fb3a26cecbed93fbea45d87dd4e68c20af0a8
Leonel Liesenberg <liesenberg.leonel@gmail.com>
2017-09-04 21:19:07 -0300

    53. Using an Assertion Library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,34 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var result = utils.add(33, 11);
 
-    if (result !== 44) {
-        throw new Error(`Expected 44, but got ${result}.`);
-    }
+    expect(result).toBe(44).toBeA('number');
 });
 
 it('should square a number', ()=>{
     var result = utils.square(2);
 
-    if (result !== 4) {
-        throw new Error(`Expected 4, but got ${result}.`);
-    }
+    expect(result).toBe(4).toBeA('number');
+});
+
+it('should verify first and last names', () => {
+    var user = { location: 'Brazil', age: 26 };
+    utils.setName(user, 'Leonel Liesenberg');
+
+    expect(user).toInclude({ firstName: 'Leonel', lastName: 'Liesenberg' }).toBeAn('object');
+});
+
+it('should test some values', () => {
+    var obj = { Name: 'Leonel', age: 26 };
+    var obj2 = { Name: 'leonel' };
+
+    expect(obj).toNotEqual(obj2);
+
+    var arr = [1,2,3];
+    
+    expect(arr).toExclude(4);
+
+    expect(obj).toInclude({Name:'Leonel'});
 });
